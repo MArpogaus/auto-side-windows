0f56276fe3a3de30db092d269971067a40ef3c98
Marcel Arpogaus <38564291+MArpogaus@users.noreply.github.com>
2025-03-28 10:36:21 +0100

    feat: add hooks and refactor display commands

--- auto-side-windows.el
+++ auto-side-windows.el
@@ -33,144 +33,144 @@
 
 (defcustom auto-side-windows-top-buffer-names nil
   "List of buffer name regexps to be displayed in top side windows.
-Each regexp is used to match buffer names. When a buffer's name 
-matches any regex in this list, the buffer will be shown in the 
+Each regexp is used to match buffer names. When a buffer's name
+matches any regex in this list, the buffer will be shown in the
 top side window."
   :type '(repeat string)
   :group 'auto-side-windows)
 
 (defcustom auto-side-windows-bottom-buffer-names nil
   "List of buffer name regexps to be displayed in bottom side windows.
-Each regexp is used to match buffer names. When a buffer's name 
-matches any regex in this list, the buffer will be shown in the 
+Each regexp is used to match buffer names. When a buffer's name
+matches any regex in this list, the buffer will be shown in the
 bottom side window."
   :type '(repeat string)
   :group 'auto-side-windows)
 
 (defcustom auto-side-windows-left-buffer-names nil
   "List of buffer name regexps to be displayed in left side windows.
-Each regexp is used to match buffer names. When a buffer's name 
-matches any regex in this list, the buffer will be shown in the 
+Each regexp is used to match buffer names. When a buffer's name
+matches any regex in this list, the buffer will be shown in the
 left side window."
   :type '(repeat string)
   :group 'auto-side-windows)
 
 (defcustom auto-side-windows-right-buffer-names nil
   "List of buffer name regexps to be displayed in right side windows.
-Each regexp is used to match buffer names. When a buffer's name 
-matches any regex in this list, the buffer will be shown in the 
+Each regexp is used to match buffer names. When a buffer's name
+matches any regex in this list, the buffer will be shown in the
 right side window."
   :type '(repeat string)
   :group 'auto-side-windows)
 
 (defcustom auto-side-windows-top-buffer-modes nil
   "List of major modes for buffers to be displayed in top side windows.
-When a buffer's major mode matches any symbol in this list, 
+When a buffer's major mode matches any symbol in this list,
 it will be shown in the top side window."
   :type '(repeat symbol)
   :group 'auto-side-windows)
 
 (defcustom auto-side-windows-bottom-buffer-modes nil
   "List of major modes for buffers to be displayed in bottom side windows.
-When a buffer's major mode matches any symbol in this list, 
+When a buffer's major mode matches any symbol in this list,
 it will be shown in the bottom side window."
   :type '(repeat symbol)
   :group 'auto-side-windows)
 
 (defcustom auto-side-windows-left-buffer-modes nil
   "List of major modes for buffers to be displayed in left side windows.
-When a buffer's major mode matches any symbol in this list, 
+When a buffer's major mode matches any symbol in this list,
 it will be shown in the left side window."
   :type '(repeat symbol)
   :group 'auto-side-windows)
 
 (defcustom auto-side-windows-right-buffer-modes nil
   "List of major modes for buffers to be displayed in right side windows.
-When a buffer's major mode matches any symbol in this list, 
+When a buffer's major mode matches any symbol in this list,
 it will be shown in the right side window."
   :type '(repeat symbol)
   :group 'auto-side-windows)
 
 (defcustom auto-side-windows-top-extra-conditions '((category . force-side-top))
   "Lists of extra conditions to match top buffers.
-These extra conditions are checked along with buffer name and major mode 
+These extra conditions are checked along with buffer name and major mode
 rules to determine if a buffer should be displayed in a top side window."
   :type '(repeat symbol)
   :group 'auto-side-windows)
 
 (defcustom auto-side-windows-bottom-extra-conditions '((category . force-side-bottom))
   "Lists of extra conditions to match bottom buffers.
-These extra conditions are checked along with buffer name and major mode 
+These extra conditions are checked along with buffer name and major mode
 rules to determine if a buffer should be displayed in a bottom side window."
   :type '(repeat symbol)
   :group 'auto-side-windows)
 
 (defcustom auto-side-windows-left-extra-conditions '((category . force-side-left))
   "Lists of extra conditions to match left buffers.
-These extra conditions are checked along with buffer name and major mode 
+These extra conditions are checked along with buffer name and major mode
 rules to determine if a buffer should be displayed in a left side window."
   :type '(repeat symbol)
   :group 'auto-side-windows)
 
 (defcustom auto-side-windows-right-extra-conditions '((category . force-side-right))
   "Lists of extra conditions to match right buffers.
-These extra conditions are checked along with buffer name and major mode 
+These extra conditions are checked along with buffer name and major mode
 rules to determine if a buffer should be displayed in a right side window."
   :type '(repeat symbol)
   :group 'auto-side-windows)
 
 (defcustom auto-side-windows-top-window-parameters nil
   "Custom window parameters for top side windows.
-This alist can be used to specify parameters like the height 
+This alist can be used to specify parameters like the height
 or width of the top side window."
   :type 'alist
   :group 'auto-side-windows)
 
 (defcustom auto-side-windows-bottom-window-parameters nil
   "Custom window parameters for bottom side windows.
-This alist can be used to specify parameters like the height 
+This alist can be used to specify parameters like the height
 or width of the bottom side window."
   :type 'alist
   :group 'auto-side-windows)
 
 (defcustom auto-side-windows-left-window-parameters nil
   "Custom window parameters for left side windows.
-This alist can be used to specify parameters like the height 
+This alist can be used to specify parameters like the height
 or width of the left side window."
   :type 'alist
   :group 'auto-side-windows)
 
 (defcustom auto-side-windows-right-window-parameters nil
   "Custom window parameters for right side windows.
-This alist can be used to specify parameters like the height 
+This alist can be used to specify parameters like the height
 or width of the right side window."
   :type 'alist
   :group 'auto-side-windows)
 
 (defcustom auto-side-windows-top-alist '((window-height . (lambda (win) (fit-window-to-buffer win 0.5 5))))
   "Custom alist for top side windows.
-This alist contains display properties which will be applied 
+This alist contains display properties which will be applied
 when displaying buffers in the top side window."
   :type 'alist
   :group 'auto-side-windows)
 
 (defcustom auto-side-windows-bottom-alist nil
   "Custom alist for bottom side windows.
-This alist contains display properties which will be applied 
+This alist contains display properties which will be applied
 when displaying buffers in the bottom side window."
   :type 'alist
   :group 'auto-side-windows)
 
 (defcustom auto-side-windows-left-alist nil
   "Custom alist for left side windows.
-This alist contains display properties which will be applied 
+This alist contains display properties which will be applied
 when displaying buffers in the left side window."
   :type 'alist
   :group 'auto-side-windows)
 
 (defcustom auto-side-windows-right-alist '((window-width . 80))
   "Custom alist for right side windows.
-This alist contains display properties which will be applied 
+This alist contains display properties which will be applied
 when displaying buffers in the right side window."
   :type 'alist
   :group 'auto-side-windows)
@@ -179,14 +179,14 @@ when displaying buffers in the right side window."
                                                         (tab-line-format . none)
                                                         (mode-line-format . none))
   "Custom window parameters for all side windows.
-These parameters will be applied to all side windows created by 
+These parameters will be applied to all side windows created by
 `auto-side-windows-mode'."
   :type 'alist
   :group 'auto-side-windows)
 
 (defcustom auto-side-windows-common-alist '((dedicated . t))
   "Custom alist for all side windows.
-These parameters will be applied to all side windows created by 
+These parameters will be applied to all side windows created by
 `auto-side-windows-mode`."
   :type 'alist
   :group 'auto-side-windows)
@@ -195,6 +195,34 @@ These parameters will be applied to all side windows created by
   "Allow reuse of side windows for same mode on given sides.
 If set, side windows may be reused for buffers of the same major mode."
   :type 'alist
+  :group 'auto-side-windows)
+
+(defcustom auto-side-windows-before-display-hook nil
+  "Hook run before displaying a buffer in a side window.
+This hook allows users to execute custom code or functions
+before a buffer is placed in a side window."
+  :type 'hook
+  :group 'auto-side-windows)
+
+(defcustom auto-side-windows-after-display-hook nil
+  "Hook run after displaying a buffer in a side window.
+This hook allows users to execute custom code or functions
+after a buffer has been placed in a side window."
+  :type 'hook
+  :group 'auto-side-windows)
+
+(defcustom auto-side-windows-before-toggle-hook nil
+  "Hook run before toggling the display of a buffer.
+This hook allows users to execute custom code or functions
+before the toggle action of a buffer in a side window."
+  :type 'hook
+  :group 'auto-side-windows)
+
+(defcustom auto-side-windows-after-toggle-hook nil
+  "Hook run after toggling the display of a buffer.
+This hook allows users to execute custom code or functions
+after the toggle action of a buffer in a side window."
+  :type 'hook
   :group 'auto-side-windows)
 
 ;; Internal Variables
@@ -213,13 +241,14 @@ additional conditions to refine the matching process."
   (let ((modes-cond `(or ,@(mapcar (lambda (mode) `(derived-mode . ,mode)) majormodes))))
     (when buffernames (setq modes-cond `(or (or ,@buffernames) ,modes-cond)))
     (setq modes-cond (append modes-cond extra-conds))
-  modes-cond))
+    modes-cond))
 
 (defun auto-side-windows--get-buffer-side (buffer &optional args)
   "Determine which side BUFFER should be displayed in.
-This function checks the buffer against user-defined conditions 
-relative to the side windows. It returns 'top, 'bottom, 'left, or 'right, 
-or nil if no conditions are met."
+This function checks the buffer against user-defined conditions
+relative to the side windows. It returns 'top, 'bottom, 'left, or 'right,
+or nil if no conditions are met.
+Optional ARGS may contain a category (New in Emacs>30.1)."
   (cond
    ((buffer-match-p `(and (not ,@(append auto-side-windows-left-extra-conditions
                                          auto-side-windows-right-extra-conditions
@@ -265,8 +294,8 @@ or nil if no conditions are met."
 
 (defun auto-side-windows--get-next-free-slot (side)
   "Return the next free slot number for SIDE.
-Each side window can have multiple slots numbered from 0 to 
-MAX-SLOTS-1. This function finds and returns the next available 
+Each side window can have multiple slots numbered from 0 to
+MAX-SLOTS-1. This function finds and returns the next available
 slot number for use."
   (let* ((max-slots (nth (cond ((eq side 'left) 0)
                                ((eq side 'top) 1)
@@ -289,8 +318,11 @@ slot number for use."
 (defun auto-side-windows--display-buffer (buffer alist)
   "Custom display buffer function for `auto-side-windows-mode'.
 BUFFER is the buffer to display and ALIST contains display parameters.
-This function determines the appropriate side for the buffer and 
-displays it in the selected side window if conditions are met."
+This function determines the appropriate side for the buffer and
+displays it in the selected side window if conditions are met.
+
+Before displaying the buffer, it runs `auto-side-windows-before-display-hook'.
+After displaying the buffer, it runs `auto-side-windows-after-display-hook'."
   (when-let* ((side (auto-side-windows--get-buffer-side buffer `(nil . ,alist)))
               (slot (auto-side-windows--get-next-free-slot side))
               (window-params (append auto-side-windows-common-window-parameters
@@ -302,17 +334,23 @@ displays it in the selected side window if conditions are met."
                              `((side . ,side)
                                (slot . ,slot)
                                (window-parameters . ,window-params)))))
+    (run-hooks 'auto-side-windows-before-display-hook)
     (unless (when (alist-get side auto-side-windows-reuse-mode-window)
               (display-buffer-reuse-mode-window buffer alist))
-      (display-buffer-in-side-window buffer alist))))
+      (display-buffer-in-side-window buffer alist))
+    (run-hooks 'auto-side-windows-after-display-hook)))
 
 ;; Commands
 
 (defun auto-side-windows-toggle-side-window nil
   "Toggle the current buffer as a side window.
-If the current window is already a side window, it will delete 
-the window. If not, the buffer will be displayed in a side window."
+If the current window is already a side window, it will delete
+the window. If not, the buffer will be displayed in a side window.
+
+Before toggling the buffer, it runs `auto-side-windows-before-toggle-hook'.
+After toggling the buffer, it runs `auto-side-windows-after-toggle-hook'."
   (interactive)
+  (run-hooks 'auto-side-windows-before-toggle-hook)
   (let ((window (selected-window))
         (buf (current-buffer)))
     (with-selected-window window
@@ -330,39 +368,44 @@ the window. If not, the buffer will be displayed in a side window."
           (switch-to-prev-buffer window 'bury)
           (display-buffer buf)))
        (t
-        (error "Not a side window"))))))
+        (error "Not a side window")))))
+  (run-hooks 'auto-side-windows-after-toggle-hook))
+
+(defun auto-side-windows-display-buffer-on-side (side)
+  "Display the current buffer in a window on SIDE.
+This command explicitly places the buffer in the specified side window.
+It runs `auto-side-windows-before-display-hook` before displaying the buffer
+and `auto-side-windows-after-display-hook` after."
+  (interactive (list (intern (completing-read "Select side: " '("left" "right" "top" "bottom")))))
+  (let ((buf (current-buffer))
+        (alist `(nil . ((category . ,(intern (concat "force-side-" (symbol-name side))))))))
+    (run-hooks 'auto-side-windows-before-display-hook)
+    (if-let* ((window (selected-window))
+              (window-side (window-parameter window 'window-side)))
+        (delete-window window)
+      (switch-to-prev-buffer window 'bury))
+    (display-buffer buf alist)
+    (run-hooks 'auto-side-windows-after-display-hook)))
 
 (defun auto-side-windows-display-buffer-top ()
-  "Display the current buffer in a top side window.
-This command explicitly places the buffer in the top side window 
-if enabled via `auto-side-windows-mode'."
+  "Display the current buffer in a top side window."
   (interactive)
-  (let ((buf (current-buffer)))
-    (display-buffer buf '(nil . ((category . force-side-top))))))
+  (auto-side-windows-display-buffer-on-side 'top))
 
 (defun auto-side-windows-display-buffer-bottom ()
-  "Display the current buffer in a bottom side window.
-This command explicitly places the buffer in the bottom side window 
-if enabled via `auto-side-windows-mode'."
+  "Display the current buffer in a bottom side window."
   (interactive)
-  (let ((buf (current-buffer)))
-    (display-buffer buf '(nil . ((category . force-side-bottom))))))
+  (auto-side-windows-display-buffer-on-side 'bottom))
 
 (defun auto-side-windows-display-buffer-left ()
-  "Display the current buffer in a left side window.
-This command explicitly places the buffer in the left side window 
-if enabled via `auto-side-windows-mode'."
+  "Display the current buffer in a left side window."
   (interactive)
-  (let ((buf (current-buffer)))
-    (display-buffer buf '(nil . ((category . force-side-left))))))
+  (auto-side-windows-display-buffer-on-side 'left))
 
 (defun auto-side-windows-display-buffer-right ()
-  "Display the current buffer in a right side window.
-This command explicitly places the buffer in the right side window 
-if enabled via `auto-side-windows-mode'."
+  "Display the current buffer in a right side window."
   (interactive)
-  (let ((buf (current-buffer)))
-    (display-buffer buf '(nil . ((category . force-side-right))))))
+  (auto-side-windows-display-buffer-on-side 'right))
 
 ;; Minor Mode
 ;;;###autoload
